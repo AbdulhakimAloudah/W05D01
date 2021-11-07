66cb272bb9c0f9244a8943903783abe933b612a0
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-07 16:37:04 +0300

    fh

--- App.js
+++ App.js
@@ -2,38 +2,35 @@ let express = require("express");
 let app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
 let todo = [
   { id: 0, title: "play" },
   { id: 1, title: "sleep" },
 ];
 
-// app.get('/', (req,res)=>{
-//     res.send('welcome')
-// })
+app.post("/", (req, res) => {
+  let newTd = {
+    id: req.body.id,
+    title: req.body.title,
+  };
+  todo.push(newTd);
+  res.json(todo);
+});
 
-// app.get('/corse/dyy',(req,res)=>{
-// res.send('hlaa')
-// })
-
-
-
-// app.post("/tasks/:id/:title", (req, res) => {
-//   const todoN = { id: req.params.id, title: req.params.title };
-//   todo.push(todoN);
-//   res.json(todo);
-// });
-
-
-app.post('/',(req,res)=>{
-let newTd = {
-  id: req.body.id,
-  title: req.body.title,
-};
-todo.push(newTd)
-res.json(todo)
-})
-
-
-
+app.delete("/:id", (req, res) => {
+  let found = todo.find((item) => {
+    return item.id === parseInt(req, params.id);
+  });
+  if (found) {
+    let targeIndex = todo.indexOf(found);
+    todo.splice(targeIndex, 1);
+    res.send(todo);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 app.listen(3000);
