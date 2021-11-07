a3c5e604aa0cd03c178ff7334a0c20136534b2bf
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-07 15:03:43 +0300

    sd

--- App.js
+++ App.js
@@ -7,12 +7,6 @@ let todo = [
   { id: 1, title: "sleep" },
 ];
 
-app.post("/tasks/:id/:title", (req, res) => {
-  const todoN = { id: req.params.id, title: req.params.title };
-  todo.push(todoN);
-  res.json(todo);
-});
-
 // app.get('/', (req,res)=>{
 //     res.send('welcome')
 // })
@@ -20,5 +14,26 @@ app.post("/tasks/:id/:title", (req, res) => {
 // app.get('/corse/dyy',(req,res)=>{
 // res.send('hlaa')
 // })
+
+
+
+// app.post("/tasks/:id/:title", (req, res) => {
+//   const todoN = { id: req.params.id, title: req.params.title };
+//   todo.push(todoN);
+//   res.json(todo);
+// });
+
+
+app.post('/',(req,res)=>{
+let newTd = {
+  id: req.body.id,
+  title: req.body.title,
+};
+todo.push(newTd)
+res.json(todo)
+})
+
+
+
 
 app.listen(3000);
